791c2f5b289ee25df56de416c60afb53f8ce762f
the-telltale-heart <chris.farlee@gmail.com>
2026-04-08 23:01:34 -0700

    Fixed rock paper scissors. Now it works!

--- index.js
+++ index.js
@@ -1,103 +1,96 @@
 // The program that does the rock-paper-scissors
 
-// javascript can't be normal and do if (0 > x < 1) {
-// console.log(why don't you logic like almost every other language I've tried.)}
-
 // Starts multiple rounds.
 // function multipleRounds() {
-// Starting counts
-// let playerScore = 0;
-// let computerScore = 0;
-// let round = 1;
-// Template strings.
-// do {
-// Formulas that don't change.
-// const resultTemplate = `Round: ${round} Player score: ${playerScore} Computer score: ${computerScore}`;
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+  // Starting counts
+  let playerScore = 0;
+  let computerScore = 0;
+  let round = 1;
+  // Template strings.
+  do {
+    // Formulas that don't change.
+    const resultTemplate = `Round: ${round} Player score: ${playerScore} Computer score: ${computerScore}`;
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-// playRound(humanSelection, computerSelection);  // Displays who won.
-// console.log(resultTemplate);
-// }  while (round < 6)
+    playRound(humanSelection, computerSelection);  // Displays who won.
+    console.log(resultTemplate);
+  } while (round < 6)
 // }
 
 
 // Function that should run when a tie is true.
-// function Tie() { 
-// const tie = "You tied with the computer.";
-// computerScore;
-// playerScore;
-// round++;
-// console.log(tie);
-// return {computerScore, playerScore, round};    
-// }
+function Tie() {
+  const tie = "You tied with the computer.";
+  computerScore; // No change
+  playerScore;  // No change
+  round++;  // Increases round counter.
+  console.log(tie);
+  return { computerScore, playerScore, round };
+}
 
 // Function that triggers when a playerWin is True.
-// function playerWin () {  // Uncaught ReferenceError: computerScore is not defined.
-// const playerWin = "You won!";
-// computerScore;
-// playerScore++;
-// round++;
-// console.log(playerWin);
-// return {computerScore, playerScore, round};
-// }
+function playerWin() {  // Uncaught ReferenceError: computerScore is not defined.
+  const playerWin = "You won!";
+  computerScore;  // No change
+  playerScore++;  // Player score goes up.
+  round++;  // Increases round counter.
+  console.log(playerWin);
+  return { computerScore, playerScore, round };
+}
 
 // Function that triggers when a computerWin is true.
-// function computerWin () {
-// const computerWin = "You lost to the computer.";
-// computerScore++;
-// playerScore;
-// round++;
-// console.log(computerWin);
-// return {computerScore, playerScore, round};
-// }
+function computerWin() {
+  const computerWin = "You lost to the computer.";
+  computerScore++;  // Computer score goes up.
+  playerScore;  // No change
+  round++;  // Increases round counter.
+  console.log(computerWin);
+  return { computerScore, playerScore, round };
+}
 
-// function playRound(humanChoice, computerChoice) {
-// let result = "";
-// Checks for tie.
-// if (humanChoice === computerChoice) {
-// result = Tie();
-// console.log(result);
-// return result;
-// }
-// Check who won.
-// else if (humanChoice === "rock" && computerChoice === "paper") {
-// result = computerWin();
-// console.log(result);
-// return result;
-// } else if (humanChoice === "rock" && computerChoice === "scissors") {
-// result = playerScore();
-// console.log(result);
-// return result;
-// } else if (humanChoice === "paper" && computerChoice === "scissors") {
-// result = computerWin();
-// console.log(result);
-// return result;
-// } else if (humanChoice === "paper" && computerChoice === "rock") {
-// result = playerWin();
-// console.log(result);
-// return result;
-// } else if (humanChoice === "scissors" && computerChoice === "rock") {
-// result = computerWin();
-// console.log(result);
-// return result;
-// } else if (humanChoice === "scissors" && computerChoice === "paper") {
-// result = playerWin(); 
-// console.log(result);
-// } else {
-// result = console.log("Error in playRound function.");
-// return result;
-// };
-// }
+function playRound(humanSelection, computerSelection) {
+  let result = "";
+  // Checks for tie.
+  if (humanSelection === computerSelection) {
+    result = Tie();
+    console.log(result);
+    return result;
+  }
+  // Check who won.
+  else if (humanSelection === "rock" && computerSelection === "paper") {
+    result = computerWin();
+    return result;
+  } else if (humanSelection === "rock" && computerSelection === "scissors") {
+    result = playerWin();
+    return result;
+  } else if (humanSelection === "paper" && computerSelection === "scissors") {
+    result = computerWin();
+    return result;
+  } else if (humanSelection === "paper" && computerSelection === "rock") {
+    result = playerWin();
+    return result;
+  } else if (humanSelection === "scissors" && computerSelection === "rock") {
+    result = computerWin();
+    return result;
+  } else if (humanSelection === "scissors" && computerSelection === "paper") {
+    result = playerWin();
+  } else {
+    // Error triggered.
+    result = console.log("Error in playRound function.");
+    return result;
+  }
+}
 
 // Function to get the semi-rng and return the computer's choice as a string of "rock", "paper", or "scissors".
-
-
 function getComputerChoice() {
+  
   // Computer's choice semi-RNG
   let computerChoice = Math.random();
+  
   // What to display when there is an unexpected outcome.
   let computerChoiceUnexpectedOutcome = "There has been an unexpected outcome in the getComputerChoice function.";
+  
   // The if statement converts the RNG computerChoice to "rock","paper", or "scissors". 
   if (computerChoice >= 0 && computerChoice <= .33) {
     computerChoice = "rock";
@@ -110,12 +103,10 @@ function getComputerChoice() {
     return computerChoice;
   } else {
     // Checks for unexpected outcomes.
-    // I only seem to get scissors or undefined as a result.
-    console.log(computerChoiceUnexpectedOutcome);
+    // Occasionally returns an undefined choice.
+    return console.log(computerChoiceUnexpectedOutcome);
   }
 }
-
-const computerSelection = getComputerChoice();
 
 function getHumanChoice() {
   let input = prompt("Which option do you chose \(Rock, Paper, or Scissors\)").toLowerCase();
@@ -131,12 +122,8 @@ function getHumanChoice() {
       let humanChoice = "scissors";
       return humanChoice;
     }
-  } while (input === "rock" || "paper" || "scissors" && input !== null && input !== undefined)
+  } while (input === "rock" && input !== null && input !== undefined || input === "paper" && input !== null && input !== undefined || input === "scissors" && input !== null && input !== undefined)
 }
-
-const humanSelection = getHumanChoice();
-
+playRound(humanSelection, computerSelection);
 // displays choices:
-console.log(humanSelection);
-console.log(computerSelection);
 // multipleRounds();
